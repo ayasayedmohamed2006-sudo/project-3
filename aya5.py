2e18192028860ab30e9fa6e793dc5e5835460333
ayasayedmohamed2006-sudo <ayasayedmohamed2006@gmail.com>
2025-09-09 02:49:58 +0300

    Add files via upload

---
+++ aya5.py
@@ -0,0 +1,10 @@
+list1={"name":'omar','grades':[70,80,90]}
+list2={"name":"mostafa","grades":[90,50,80]}
+list3={"name":"mohamed",'grades':[80,95,70]}
+
+x=sum(list1["grades"])/3
+y=sum(list2["grades"])/3
+z=sum(list3["grades"])/3
+print('name:omar\n average grade:',x)
+print('name:mostafa\n average grade:',y)
+print('name:mohamed\n average grade:',z)
